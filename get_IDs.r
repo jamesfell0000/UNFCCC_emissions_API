70ab5c065e311e33b8871fc6125452f43bf5ca71
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-06-25 19:46:37 +1000

    Create get_IDs.r

---
+++ get_IDs.r
@@ -0,0 +1,8 @@
+#How to get the ID numbers for the countries:
+df_parties <- get_parties()
+
+#How to get the ID numbers for the years:
+df_yearIDs <- get_years()
+
+#How to get the ID numbers for the variables:
+df_variableIDs <- get_variableIDs()
