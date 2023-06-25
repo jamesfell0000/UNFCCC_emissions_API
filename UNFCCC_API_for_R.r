9c40ee4ea0f625de53c866f3c921a4dddb96e923
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-06-25 19:57:34 +1000

    Update UNFCCC_API_for_R.r

--- UNFCCC_API_for_R.r
+++ UNFCCC_API_for_R.r
@@ -139,36 +139,9 @@ return(results)
 }
 
 ##EXAMPLE OF QUERY:
-#First define what parties, years and variables:
-parties_list = list(36) #A list of parties. You can get these IDs by running parties<-get_parties()
-years_list = list(58,59,60,61,62) #A list of years. You can get these IDs by running years<-get_years()
-variables_list = list(200187) #A list of variable IDs. You can get these IDs by running variableIDs<-get_variableIDs()
-#Then run the query:
-query_results <- flex_query(parties_list,years_list,variables_list)
-
-
-
-#Or you can do all years:
-years<-get_years()
-years_list = as.list(years$yearId)
-
-#Or you can do all variables:
-variableIDs<-get_variableIDs()
-variables_list = as.list(variableIDs$variableId)
-variables_list<-split(variables_list, ceiling(seq_along(variables_list)/2000))
-years_list = list(62)
-parties_list = list(36)
-query_results <- flex_query(parties_list,years_list,variables_list[[1]])
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[2]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[3]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[4]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[5]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[6]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[7]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[8]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[9]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[10]]))
-query_results <- rbind(query_results,flex_query(parties_list,years_list,variables_list[[11]]))
-
-
-
+##First define what parties, years and variables:
+#parties_list = list(36) #A list of parties. You can get these IDs by running parties<-get_parties()
+#years_list = list(58,59,60,61,62) #A list of years. You can get these IDs by running years<-get_years()
+#variables_list = list(200187) #A list of variable IDs. You can get these IDs by running variableIDs<-get_variableIDs()
+##Then run the query:
+#query_results <- flex_query(parties_list,years_list,variables_list)
