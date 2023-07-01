9edcf5e1846f152bd93180a31fa61a5cfb695ea9
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-07-01 16:58:06 +1000

    Update install.r

--- install.r
+++ install.r
@@ -1,2 +1,3 @@
 #Installation
 download.file("https://raw.githubusercontent.com/jamesfell0000/UNFCCC_emissions_API/main/UNFCCC_API_for_R.r", "UNFCCC_API_for_R.r",cacheOK = FALSE)
+install.packages(c("httr", "jsonlite","rrapply","dplyr"))
