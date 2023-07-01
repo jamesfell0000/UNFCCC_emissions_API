c8a6e4118fc82a4c77d44512fe2d325609e9e746
jamesfell0000 <69333398+jamesfell0000@users.noreply.github.com>
2023-07-01 16:41:14 +1000

    Update UNFCCC_API_for_R.r

--- UNFCCC_API_for_R.r
+++ UNFCCC_API_for_R.r
@@ -3,10 +3,21 @@ library(jsonlite)
 library(rrapply)
 library(dplyr)
 
+#Get cookies first:
+get_cookies <- function() {
+UNFCCC_headers <- c("User-Agent" = "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36","Accept" = "*/*","Accept-Encoding" = "gzip, deflate, br","Accept-Language" = "en-US,en;q=0.9")
+r <- GET("https://di.unfccc.int/api/parties/annexOne", add_headers(.headers = UNFCCC_headers))
+ck <- cookies(r)$value
+return(ck)
+}
+
+
 #Get a list of all the parties
 get_parties <- function() {
 #Get annex one countries, just as a reference table
-r <- GET("https://di.unfccc.int/api/parties/annexOne")
+UNFCCC_headers <- c("User-Agent" = "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36","Accept" = "*/*","Accept-Encoding" = "gzip, deflate, br","Accept-Language" = "en-US,en;q=0.9")
+if (exists('ck')==FALSE) {ck <- get_cookies()}
+r <- GET("https://di.unfccc.int/api/parties/annexOne", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 annex_1_parties <- fromJSON(content(r, "text"))
 annex_1_parties <- annex_1_parties[[3]][[2]]
 colnames(annex_1_parties)[which(names(annex_1_parties) == "id")] <- "partyId"
@@ -14,7 +25,7 @@ colnames(annex_1_parties)[which(names(annex_1_parties) == "code")] <- "partyCode
 colnames(annex_1_parties)[which(names(annex_1_parties) == "name")] <- "partyDesc"
 
 #Get non annex one countries, just as a reference table
-r <- GET("https://di.unfccc.int/api/parties/nonAnnexOne")
+r <- GET("https://di.unfccc.int/api/parties/nonAnnexOne", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 non_annex_1_parties <- fromJSON(content(r, "text"))
 non_annex_1_parties <- non_annex_1_parties[[3]][[1]]
 colnames(non_annex_1_parties)[which(names(non_annex_1_parties) == "id")] <- "partyId"
@@ -30,7 +41,9 @@ return(parties)
 
 #Get years, just as a reference table
 get_years <- function () {
-r <- GET("https://di.unfccc.int/api/years/single")
+UNFCCC_headers <- c("User-Agent" = "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36","Accept" = "*/*","Accept-Encoding" = "gzip, deflate, br","Accept-Language" = "en-US,en;q=0.9")
+if (exists('ck')==FALSE) {ck <- get_cookies()}
+r <- GET("https://di.unfccc.int/api/years/single", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 years <- fromJSON(content(r, "text"))
 years <- years[[1]]
 colnames(years)[which(names(years) == "id")] <- "yearId"
@@ -40,9 +53,10 @@ return(years)
 
 #GET A LIST OF THE VARIABLE IDs THAT YOU NEED FOR A QUERY:
 get_variableIDs <- function () {
-
+UNFCCC_headers <- c("User-Agent" = "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36","Accept" = "*/*","Accept-Encoding" = "gzip, deflate, br","Accept-Language" = "en-US,en;q=0.9")
+if (exists('ck')==FALSE) {ck <- get_cookies()}
 #Get categories, just as a reference table
-r <- GET("https://di.unfccc.int/api/dimension-instances/category")
+r <- GET("https://di.unfccc.int/api/dimension-instances/category", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 json_content<-content(r)
 melted_content <- rrapply(json_content, how = "melt")
 names <- melted_content %>% filter_all(any_vars(. %in% c('name')))
@@ -54,14 +68,14 @@ colnames(categories)[which(names(categories) == "V3")] <- "categoryDesc"
 
 
 #Get classifications, just as a reference table
-r <- GET("https://di.unfccc.int/api/dimension-instances/classification")
+r <- GET("https://di.unfccc.int/api/dimension-instances/classification", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 classifications <- fromJSON(content(r, "text"))
 classifications <- as.data.frame(classifications$annexOne)
 colnames(classifications)[which(names(classifications) == "id")] <- "classificationId"
 colnames(classifications)[which(names(classifications) == "name")] <- "classificationDesc"
 
 #Get measures, just as a reference table
-r <- GET("https://di.unfccc.int/api/dimension-instances/measure")
+r <- GET("https://di.unfccc.int/api/dimension-instances/measure", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 measures <- fromJSON(content(r, "text"))
 measures_annexOne <- do.call("rbind",list(measures[["annexOne"]][["children"]][[1]],measures[["annexOne"]][["children"]][[2]],measures[["annexOne"]][["children"]][[3]],measures[["annexOne"]][["children"]][[4]],measures[["annexOne"]][["children"]][[5]]))
 measures_nonAnnexOne <- do.call("rbind",list(measures[["nonAnnexOne"]][["children"]][[1]],measures[["nonAnnexOne"]][["children"]][[2]],measures[["nonAnnexOne"]][["children"]][[3]],measures[["nonAnnexOne"]][["children"]][[4]],measures[["nonAnnexOne"]][["children"]][[5]]))
@@ -74,24 +88,24 @@ colnames(measures)[which(names(measures) == "id")] <- "measureId"
 colnames(measures)[which(names(measures) == "name")] <- "measureDesc"
 
 #Get gases, just as a reference table
-r <- GET("https://di.unfccc.int/api/dimension-instances/gas")
+r <- GET("https://di.unfccc.int/api/dimension-instances/gas", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 gases <- fromJSON(content(r, "text"))
 gases <- as.data.frame(gases$annexOne)
 colnames(gases)[which(names(gases) == "id")] <- "gasId"
 colnames(gases)[which(names(gases) == "name")] <- "gasDesc"
 
 #Get units, just as a reference table
-r <- GET("https://di.unfccc.int/api/conversion/fq")
+r <- GET("https://di.unfccc.int/api/conversion/fq", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 units <- fromJSON(content(r, "text"))
 units <- as.data.frame(units$units)
 colnames(units)[which(names(units) == "id")] <- "unitId"
 colnames(units)[which(names(units) == "name")] <- "unitDesc"
 
 #Get variableIDs:
-r <- GET("https://di.unfccc.int/api/variables/fq/annexOne")
+r <- GET("https://di.unfccc.int/api/variables/fq/annexOne", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 variableIDs_annexOne <- as.data.frame(fromJSON(content(r, "text")))
 
-r <- GET("https://di.unfccc.int/api/variables/fq/nonAnnexOne")
+r <- GET("https://di.unfccc.int/api/variables/fq/nonAnnexOne", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 variableIDs_nonAnnexOne <- as.data.frame(fromJSON(content(r, "text")))
 
 variableIDs <- rbind(variableIDs_annexOne,variableIDs_nonAnnexOne)
@@ -121,10 +135,10 @@ return(variableIDs)
 
 
 flex_query <- function (parties_list,years_list,variables_list) {
-r <- POST("https://di.unfccc.int/api/records/flexible-queries", body = list("variableIds" = variables_list, "partyIds" = parties_list, "yearIds" = years_list),encode = "json")
+UNFCCC_headers <- c("User-Agent" = "Mozilla/5.0 (X11; CrOS x86_64 14541.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36","Accept" = "*/*","Accept-Encoding" = "gzip, deflate, br","Accept-Language" = "en-US,en;q=0.9")
+if (exists('ck')==FALSE) {ck <- get_cookies()}
+r <- POST("https://di.unfccc.int/api/records/flexible-queries", body = list("variableIds" = variables_list, "partyIds" = parties_list, "yearIds" = years_list),encode = "json", set_cookies(.cookies = ck), add_headers(.headers = UNFCCC_headers))
 results<-as.data.frame(fromJSON(content(r,"text")))
-
-
 
 #Now make the results human readable by merging in the descriptions for the variableId and the partyId and the yearId
 parties<-get_parties()
